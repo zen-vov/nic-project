f99e47de6b648cab754c67ff3c0ad0a9b586db41
zen_vov <zholamanbv@gmail.com>
2024-03-20 19:46:29 +0500

    init header, footer

--- src/widgets/Layout/Footer/Footer.tsx
+++ src/widgets/Layout/Footer/Footer.tsx
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="container">footer page</div>
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div>
+            <h3>Morent</h3>
+            <p>
+              Our vision is to provide convenience and help increase your sales
+              business.
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
